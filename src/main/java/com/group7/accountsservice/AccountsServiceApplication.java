6800667e5f50bfc7929a9de08fd15742f219c6f5
Renato <renato.garay@pucp.pe>
2022-05-03 17:26:29 -0500

    first commit

---
+++ src/main/java/com/group7/accountsservice/AccountsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.group7.accountsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsServiceApplication.class, args);
+    }
+
+}
